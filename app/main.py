50dea6f3dab25b4c24dd661aca18ddaaf4bafe62
AlgoVision <louisbaffoe00@gmail.com>
2025-09-26 01:21:26 +0000

    Update main.py and style.css

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ def add_sidebar():
     for label, key in slider_labels:
         input_dict[key] = st.sidebar.slider(
             label,
-            min_value=float(0),
+            min_value=float(data[key].min()),
             max_value=float(data[key].max()),
             value=float(data[key].mean())
         )
@@ -140,9 +140,13 @@ def add_predictions(input_data):
     model = pickle.load(open('model/model.pkl', 'rb'))
     scaler = pickle.load(open('model/scaler.pkl', 'rb'))
     
-    input_array = np.array(list(input_data.values())).reshape(1,-1)
+    data = get_clean_data()
+    X_columns = data.drop('diagnosis', axis=1).columns
     
-    input_array_scaled = scaler.transform(input_array)
+    input_array = np.array(list(input_data.values())).reshape(1, -1)
+    input_df = pd.DataFrame(input_array, columns=X_columns)
+    
+    input_array_scaled = scaler.transform(input_df)
     
     prediction = model.predict(input_array_scaled)
     
@@ -152,10 +156,10 @@ def add_predictions(input_data):
     if prediction[0] == 0:
         st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
     else:
-        st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
+        st.write("<span class='diagnosis malignant'>Malignant</span>", unsafe_allow_html=True)
     
     st.write('Probability of being benign: ', model.predict_proba(input_array_scaled)[0][0])
-    st.write('Probability of being malicious: ', model.predict_proba(input_array_scaled)[0][1])
+    st.write('Probability of being malignant: ', model.predict_proba(input_array_scaled)[0][1])
 
     st.write('This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional professional diagnosis.')
 def main():
